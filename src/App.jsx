5bf2c7ea5f8a16e4e3785adf9d327b7fd6a91875
vaibhavmca191 <vaibhavthombare430@gmail.com>
2025-03-21 12:57:23 +0530

    practice on component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Lottery from './component/Lottery'
+import TicketNum from './component/TicketNum'
+import Ticket from './component/Ticket'
+
 
 function App() {
  let col = () =>{
@@ -11,7 +14,7 @@ function App() {
 
   return (
     <>
- <Lottery/>      
+<Lottery n={3} winningSum={15}/>
     </>
   )
 }
